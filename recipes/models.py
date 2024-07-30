89ea82f5ff12eac7fd8a9952de36471d73e56022
Junior <jumiormaluleka05@gmail.com>
2024-07-30 21:29:59 +0200

    add docstrings to models.py

--- recipes/models.py
+++ recipes/models.py
@@ -2,8 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Recipe(models.Model):
+    """
+    Model representing a recipe.
+
+    Attributes:
+        name (CharField): The name of the recipe, limited to 200 characters.
+        description (TextField): A detailed description of the recipe.
+    """
     name = models.CharField(max_length=200)
     description = models.TextField()
 
     def __str__(self):
+        """
+        Return a string representation of the recipe.
+
+        Returns:
+            str: The name of the recipe.
+        """
         return self.name
